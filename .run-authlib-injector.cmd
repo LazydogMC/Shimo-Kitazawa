ed336f96117e9c93a2944ebd59e252ffd3aec1e8
Ilya Ivanovsky <88927578+IlyaIvanovsky@users.noreply.github.com>
2023-02-04 17:51:33 +0800

    Add files via upload

---
+++ .run-authlib-injector.cmd
@@ -0,0 +1,2 @@
+java -Xms4G -Xmx4G -javaagent:authlib-injector-1.2.1.jar=https://littleskin.cn/api/yggdrasil -jar paper-1.19.3-386.jar nogui
+pause
---
+++ .run.cmd
@@ -0,0 +1,2 @@
+java -Xms4G -Xmx4G -jar leaves-1.19.3.jar nogui
+pause
